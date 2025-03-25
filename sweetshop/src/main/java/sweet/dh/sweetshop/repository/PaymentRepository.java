b2d4926246edaf774cce4d68e6751f5366b00650
sweet.dh <dlsrksrhk@gmail.com>
2025-03-25 14:11:00 +0900

    DB에 결제 내역 저장하기
    - PG 대행사 결제 성공 이후 DB에 내역 저장

---
+++ sweetshop/src/main/java/sweet/dh/sweetshop/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package sweet.dh.sweetshop.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sweet.dh.sweetshop.entity.Payment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // 기본 CRUD 메서드는 JpaRepository가 제공
+    Optional<Payment> findByImpUid(String impUid);
+    List<Payment> findByPaymentDateBetweenAndStatus(LocalDateTime startDate, LocalDateTime endDate, String status);
+}
